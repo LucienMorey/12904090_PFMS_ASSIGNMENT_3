294fffee94cc2978e90dade35fe7332b375facb8
Lucien Morey <lucienp@live.com.au>
2020-05-12 14:40:27 +1000

    added in expected private variables

--- src/pure_pursuit.h
+++ src/pure_pursuit.h
@@ -1,10 +1,21 @@
 #ifndef PUREPURSUIT_H
 #define PUREPURSUIT_H
 
+#include "types.h"
+
 class PurePursuit
 {
 private:
   /* data */
+  GlobalOrd target_position_;
+
+  unsigned int max_g_;
+
+  double max_linear_velocity_;
+  double min_linear_velocity_;
+
+  const double LINEAR_TOLERANCE = 200.0;
+
 public:
   PurePursuit(/* args */);
   ~PurePursuit();
