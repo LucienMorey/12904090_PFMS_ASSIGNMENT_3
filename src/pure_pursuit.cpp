bb3a0bcd244ff0a9f827bb9e33573610019c9c14
Lucien Morey <lucienp@live.com.au>
2020-05-12 14:14:59 +1000

    created stub files for pure pursuit class

---
+++ src/pure_pursuit.cpp
@@ -0,0 +1,9 @@
+#include "pure_pursuit.h"
+
+pure_pursuit::pure_pursuit(/* args */)
+{
+}
+
+pure_pursuit::~pure_pursuit()
+{
+}
---
+++ src/pure_pursuit.h
@@ -0,0 +1,21 @@
+#ifndef PUREPURSUIT_H
+#define PUREPURSUIT_H
+
+class pure_pursuit
+{
+private:
+  /* data */
+public:
+  pure_pursuit(/* args */);
+  ~pure_pursuit();
+};
+
+pure_pursuit::pure_pursuit(/* args */)
+{
+}
+
+pure_pursuit::~pure_pursuit()
+{
+}
+
+#endif
