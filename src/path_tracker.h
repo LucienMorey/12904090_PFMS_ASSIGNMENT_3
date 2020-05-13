3f933dc97e1c7c7e7ac817f1417f8b4960735bd4
Lucien Morey <lucienp@live.com.au>
2020-05-13 18:20:21 +1000

    created parent path tracker class for pure pursuit

---
+++ src/path_tracker.h
@@ -0,0 +1,25 @@
+#ifndef PATHTRACKER_H
+#define PATHTRACKER_H
+
+#include "types.h"
+
+struct Twist_t
+{
+  double vY;
+  double vX;
+  double vZ;
+};
+
+class path_tracker
+{
+protected:
+  const double LINEAR_TOLERANCE = 200.0;
+  const double ANGULAR_TOLERANCE = M_PI / 36;
+
+public:
+  path_tracker(){};
+
+  virtual Twist_t track(Pose current_pose, Pose target_pose) = 0;
+};
+
+#endif
--- src/pure_pursuit.h
+++ src/pure_pursuit.h
@@ -1,32 +1,23 @@
 #ifndef PUREPURSUIT_H
 #define PUREPURSUIT_H
 
-#include "types.h"
+#include "path_tracker.h"
 
-struct Twist_t
-{
-  double vY;
-  double vX;
-  double vZ;
-};
-
-class PurePursuit
+class PurePursuit : public path_tracker
 {
 private:
   /* data */
+
+  double LINEAR_KP = 1.0;
+  double ANGULAR_KD = 1.0;
 
   unsigned int max_g_;
 
   double max_linear_velocity_;
   double min_linear_velocity_;
 
-  double LINEAR_KP = 1.0;
-  double ANGULAR_KD = 1.0;
-
-  const double LINEAR_TOLERANCE = 200.0;
-
 public:
-  PurePursuit(/* args */);
+  PurePursuit();
   ~PurePursuit();
   Twist_t pursue(const Pose& current_pose, const Pose& target_pose);
 };
