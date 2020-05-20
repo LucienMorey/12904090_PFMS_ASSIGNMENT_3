b509edfb35c005ba2af44903e749a9bd627b3c3e
Lucien Morey <lucienp@live.com.au>
2020-05-20 18:39:23 +1000

    made graph member prtoected for subclass

--- src/graph.h
+++ src/graph.h
@@ -5,31 +5,32 @@
 #include <vector>
 #include <map>
 
-//A undirected, weighted graph
-class Graph {
+// A undirected, weighted graph
+class Graph
+{
   typedef std::string vertex;
   typedef unsigned int weight;
 
-  //The '_t' notation is used to denote a new type
+  // The '_t' notation is used to denote a new type
   typedef std::map<vertex, weight> edges_t;
   typedef std::map<vertex, edges_t> graph_t;
 
 public:
-  //Adds the passed in vertex to the graph (with no edges).
+  // Adds the passed in vertex to the graph (with no edges).
   void addVertex(vertex);
-  //Checks if the vertex exists.
+  // Checks if the vertex exists.
   bool hasVertex(vertex);
-  //Adds an edge between the two vertices with the given weight
+  // Adds an edge between the two vertices with the given weight
   void addEdge(vertex, vertex, weight);
-  //Returns a vector containing all the vertices.
+  // Returns a vector containing all the vertices.
   std::vector<vertex> getVertices(void);
-  //Returns a vector containing the neighbours of a given vertex
+  // Returns a vector containing the neighbours of a given vertex
   std::vector<vertex> getNeighbours(vertex);
-  //Return a tree using breadth first search on the entire graph
+  // Return a tree using breadth first search on the entire graph
   std::vector<vertex> bfs(vertex);
 
-private:
-    graph_t weightedGraph_; //The data structure storing our graph.
+protected:
+  graph_t weightedGraph_;  // The data structure storing our graph.
 };
 
-#endif // GRAPH_H
+#endif  // GRAPH_H
