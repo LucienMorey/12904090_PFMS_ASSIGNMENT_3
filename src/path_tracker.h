9a51b22a200eae0204f295586bdd33e8af1a11aa
Lucien Morey <lucienp@live.com.au>
2020-05-23 17:19:37 +1000

    lowered angle tolerance treshold

--- src/path_tracker.h
+++ src/path_tracker.h
@@ -13,7 +13,7 @@ struct Twist_t
 class path_tracker
 {
 protected:
-  const double LARGE_ANGULAR_TOLERANCE = M_PI / 18;
+  const double LARGE_ANGULAR_TOLERANCE = M_PI / 36;
 
   const double MIN_LINEAR_VELOCITY = 50.0;
   const double MAX_LINEAR_VELOCITY = 900.0;
