6c893999542bb5a3a6b22c393a21dd8d08cfabe7
Lucien Morey <lucienp@live.com.au>
2020-05-24 19:08:17 +1000

    distance planner cleanup

--- src/distance_planner.cpp
+++ src/distance_planner.cpp
@@ -22,12 +22,8 @@ void DistancePlanner::plan(std::vector<Aircraft> Aircraft)
     key++;
   }
 
-  std::cout << "friendly at " << planes_.at(FRIENDLY_KEY).pose.position.x << " "
-            << planes_.at(FRIENDLY_KEY).pose.position.y << std::endl;
-
   for (auto planes_key = 1; planes_key != planes_.size(); planes_key++)
   {
-    // std::cout << planes_.at(planes_key).timer.elapsed() << std::endl;
     GlobalOrd point_next_time_step = {
       planes_.at(planes_key).pose.position.x +
           planes_.at(planes_key).linear_velocity * cos(planes_.at(planes_key).pose.orientation) *
@@ -38,15 +34,11 @@ void DistancePlanner::plan(std::vector<Aircraft> Aircraft)
     };
 
     Pose pose_next_time_step = { point_next_time_step, planes_.at(planes_key).pose.orientation };
-    // poses.push_back(pose_next_time_step);
 
-    planes_.at(planes_key).pose = pose_next_time_step;
+    planes_.at(planes_key).currentGoalPose = pose_next_time_step;
 
     double distance = sqrt(pow(pose_next_time_step.position.x - planes_.at(FRIENDLY_KEY).pose.position.x, 2) +
                            pow(pose_next_time_step.position.y - planes_.at(FRIENDLY_KEY).pose.position.y, 2));
-
-    std::cout << "bogie at " << pose_next_time_step.position.x << " " << pose_next_time_step.position.y
-              << " with distance " << distance << std::endl;
 
     double angle_to_friendly = atan2(planes_.at(FRIENDLY_KEY).pose.position.x - pose_next_time_step.position.x,
                                      planes_.at(FRIENDLY_KEY).pose.position.y - pose_next_time_step.position.y);
@@ -60,7 +52,6 @@ void DistancePlanner::plan(std::vector<Aircraft> Aircraft)
       angle_error_bogie_to_friendly = 2 * M_PI - angle_error_bogie_to_friendly;
     }
 
-    // double weight = pow(distance, angle_error_bogie_to_friendly);
     double weight = distance;
     addEdge(FRIENDLY_KEY, planes_key, weight);
   }
@@ -68,27 +59,21 @@ void DistancePlanner::plan(std::vector<Aircraft> Aircraft)
   unsigned int index_to_most_efficient_bogie = 1;
   for (int i = 1; i != weightedGraph_.at(FRIENDLY_KEY).size(); i++)
   {
-    std::cout << "ping" << std::endl;
     if (weightedGraph_.at(FRIENDLY_KEY).at(i) < weightedGraph_.at(FRIENDLY_KEY).at(index_to_most_efficient_bogie))
     {
       index_to_most_efficient_bogie = i;
     }
   }
 
-  double distance = sqrt(
-      pow(planes_.at(FRIENDLY_KEY).pose.position.x - planes_.at(index_to_most_efficient_bogie).pose.position.x, 2) +
-      pow(planes_.at(FRIENDLY_KEY).pose.position.y - planes_.at(index_to_most_efficient_bogie).pose.position.y, 2));
-
-  std::cout << "bogie chosen at " << planes_.at(index_to_most_efficient_bogie).pose.position.x << " "
-            << planes_.at(index_to_most_efficient_bogie).pose.position.y << " distance of " << distance << std::endl;
-  std::cout << std::endl;
-
-  poses.push_back(planes_.at(FRIENDLY_KEY).pose);
-  // std::cout << poses.size() << std::endl;
-  sim_->testPose(poses);
+  double distance = sqrt(pow(planes_.at(FRIENDLY_KEY).pose.position.x -
+                                 planes_.at(index_to_most_efficient_bogie).currentGoalPose.position.x,
+                             2) +
+                         pow(planes_.at(FRIENDLY_KEY).pose.position.y -
+                                 planes_.at(index_to_most_efficient_bogie).currentGoalPose.position.y,
+                             2));
 
   std::lock_guard<std::mutex> lock(path_mx_);
   path_.clear();
   path_.push_back(planes_.at(FRIENDLY_KEY).pose);
-  path_.push_back(planes_.at(index_to_most_efficient_bogie).pose);
+  path_.push_back(planes_.at(index_to_most_efficient_bogie).currentGoalPose);
 }
