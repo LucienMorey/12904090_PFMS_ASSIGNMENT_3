5c9f3409be455a5aa081993a0db254fea5841adc
Lucien Morey <lucienp@live.com.au>
2020-05-26 15:48:01 +1000

    doxygen for estimator

--- src/estimator.cpp
+++ src/estimator.cpp
@@ -1,8 +1,17 @@
 #include "estimator.h"
+
+/**
+ * @brief Construct a new Estimator:: Estimator object
+ *
+ */
 Estimator::Estimator()
 {
 }
 
+/**
+ * @brief Destroy the Estimator:: Estimator object. Joins any started estimation threads and cleans up pointer
+ *
+ */
 Estimator::~Estimator()
 {
   friendly_updater.join();
@@ -12,6 +21,12 @@ Estimator::~Estimator()
   delete updater;
 }
 
+/**
+ * @brief set a simulator to pull data from. once a simulator has been set threads obtaining data and estimating bogie
+ * pose and twist will begin
+ *
+ * @param simulator - Simulator pointer to be used for data retrieval
+ */
 void Estimator::setSimulator(Simulator* simulator)
 {
   // set simulator for updater use
@@ -23,6 +38,11 @@ void Estimator::setSimulator(Simulator* simulator)
   bogie_estimator = std::thread(&Estimator::determineBogies_, this);
 }
 
+/**
+ * @brief Thread safe getter for bogies that have been triangulated
+ *
+ * @return std::vector<Aircraft> - Vector contrainer of triangulated bogies
+ */
 std::vector<Aircraft> Estimator::getBogies()
 {
   // threadsafe getter for current matched bogies
@@ -30,6 +50,11 @@ std::vector<Aircraft> Estimator::getBogies()
   return bogies_.a;
 }
 
+/**
+ * @brief retrieve most recent sensor data and fuse it to triangulate bogies. There is no return but instead the
+ * function is designed to run within a thread and constantly triangulate bogies in real time.
+ *
+ */
 void Estimator::determineBogies_()
 {
   // condvar wait until there is at least one base reading
@@ -81,6 +106,16 @@ void Estimator::determineBogies_()
   }
 }
 
+/**
+ * @brief Create 2 line segments between old/previous and previous/current data and compare them based on length, and
+ * gradient to match bogies and determine heading and velocity in the current time step
+ *
+ * @param range_bogies_global_t1 GlobalOrdStamped - Global position of bogies current time step
+ * @param range_bogies_global_t2 GlobalOrdStamped - Global position of bogies previous time step
+ * @param range_bogies_global_t3 GlobalOrdStamped - Global position of bogies in old timestep
+ * @return std::vector<Aircraft> Vector container containing all bogies matched in the current timestep in no particular
+ * order
+ */
 std::vector<Aircraft> Estimator::matchBogies(std::vector<GlobalOrdStamped> range_bogies_global_t1,
                                              std::vector<GlobalOrdStamped> range_bogies_global_t2,
                                              std::vector<GlobalOrdStamped> range_bogies_global_t3)
@@ -161,6 +196,14 @@ std::vector<Aircraft> Estimator::matchBogies(std::vector<GlobalOrdStamped> range
   return matched_bogies;
 }
 
+/**
+ * @brief Transform helper function. Transforms local range bearing data of friendly scan to global position and assigns
+ * the position the timestamp of the data reading
+ *
+ * @param friendly_pose - Pose pose of friendly for use in transform
+ * @param relative_pos - RangeBearingStamped struct of detected bogie relative to friendly aircraft
+ * @return GlobalOrdStamped - global position of bogie with a timestamp in ms
+ */
 GlobalOrdStamped Estimator::transformBogietoGlobal(Pose friendly_pose, RangeBearingStamped relative_pos)
 {
   // return global co-ords of bogie relative to friendly
